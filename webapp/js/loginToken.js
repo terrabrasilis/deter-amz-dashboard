ade3723962c500b507a52da3572e31a818a4efad
Paulo Luan <pauloluan.inova@gmail.com>
2019-09-13 10:15:46 -0300

    added parseJWT token function

--- webapp/js/loginToken.js
+++ webapp/js/loginToken.js
@@ -27,6 +27,16 @@ var Token={
     this.setKey(this.expiredKey,state);
   },
 
+  parseJwt(token) {
+      var base64Url = token.split('.')[1]
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+      }).join(''))
+
+      return JSON.parse(jsonPayload)
+  },
+
   /**
    * Set a new value for the specified key.
    * To remove one key, set the value with null or undefined
