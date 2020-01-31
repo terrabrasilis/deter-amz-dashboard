1815315e9f8364de98990f7697ae3b3e47b9af4d
André Carvalho <afacarvalho@yahoo.com.br>
2020-01-31 18:08:37 -0200

    Improve the build script to auto detect version from a file

--- docker-build.sh
+++ docker-build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+VERSION=$(cat PROJECT_VERSION | grep -oP '(?<="version": ")[^"]*')
+
 # Stopping all containers
 #docker container stop terrabrasilis_amazon_alert_daily
 #docker container stop terrabrasilis_amazon_alert_aggregated
 
 # build all images
-docker build -t terrabrasilis/amazon-alert-daily:v2.5 --build-arg INDEX_FILE=deter-amazon-daily -f environment/Dockerfile .
-docker build -t terrabrasilis/amazon-alert-aggregated:v2.5 --build-arg INDEX_FILE=deter-amazon-aggregated -f environment/Dockerfile .
+docker build -t terrabrasilis/amazon-alert-daily:v$VERSION --build-arg INDEX_FILE=deter-amazon-daily -f environment/Dockerfile .
+docker build -t terrabrasilis/amazon-alert-aggregated:v$VERSION --build-arg INDEX_FILE=deter-amazon-aggregated -f environment/Dockerfile .
 
 # send to dockerhub
 ## docker login
-docker push terrabrasilis/amazon-alert-daily:v2.5
-docker push terrabrasilis/amazon-alert-aggregated:v2.5
+docker push terrabrasilis/amazon-alert-daily:v$VERSION
+docker push terrabrasilis/amazon-alert-aggregated:v$VERSION
 
 # If you want run containers, uncomment this lines
-#docker run -d --rm -p 83:80 --name terrabrasilis_amazon_alert_daily terrabrasilis/amazon-alert-daily:v2.2-beta
-#docker run -d --rm -p 84:80 --name terrabrasilis_amazon_alert_aggregated terrabrasilis/amazon-alert-aggregated:v2.2-beta
+#docker run -d --rm -p 83:80 --name terrabrasilis_amazon_alert_daily terrabrasilis/amazon-alert-daily:v$VERSION
+#docker run -d --rm -p 84:80 --name terrabrasilis_amazon_alert_aggregated terrabrasilis/amazon-alert-aggregated:v$VERSION
