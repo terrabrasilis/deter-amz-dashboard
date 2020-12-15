f6c6f3f42c7c8f3d036224d6ff2f8da1a6f413b4
André Carvalho <afacarvalho@yahoo.com.br>
2020-12-15 18:55:44 -0300

    Changes to load new data for coverage clouds

--- webapp/data/curl_get_json.sh
+++ webapp/data/curl_get_json.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # to test cloud monthly data
-#URL="http://terrabrasilis.dpi.inpe.br/geoserver/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=deter-amz:cloud_m_d&OUTPUTFORMAT=application%2Fjson"
-#curl "$URL" -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en,en-US;q=0.5' --compressed -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > deter-amazon-cloud-month.json
+URL="http://terrabrasilis.dpi.inpe.br/geoserver/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=deter-amz:cloud_m_d&OUTPUTFORMAT=application%2Fjson"
+curl "$URL" -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > deter-amazon-cloud-month.json
 
 # download daily data to work local
 #URL="http://terrabrasilis.dpi.inpe.br/geoserver/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=deter-amz:deter_daily_d&OUTPUTFORMAT=application%2Fjson"
