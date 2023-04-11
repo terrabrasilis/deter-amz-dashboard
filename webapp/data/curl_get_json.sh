203d0905c321f511905a3b6210d82c1041aff4e2
andre-carvalho <afacarvalho@yahoo.com.br>
2023-04-11 16:24:00 -0300

    Download data in CSV format

--- webapp/data/curl_get_json.sh
+++ webapp/data/curl_get_json.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+BASE_URL="http://terrabrasilis.dpi.inpe.br/geoserver/deter-amz"
+
+# download daily data to work local
+URL="${BASE_URL}/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=all_daily_d&OUTPUTFORMAT=csv"
+curl "$URL" -H 'User-Agent: Shell script with CURL' --compressed -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > deter-amazon-daily.csv
+
+URL="${BASE_URL}/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=updated_date&OUTPUTFORMAT=application%2Fjson"
+curl "$URL" -H 'User-Agent: Shell script with CURL' --compressed -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > updated-date.json
+exit
 
 # to test cloud monthly data
-URL="http://terrabrasilis.dpi.inpe.br/geoserver/deter-amz/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=cloud_m_d&OUTPUTFORMAT=application%2Fjson"
+URL="${BASE_URL}/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=cloud_m_d&OUTPUTFORMAT=application%2Fjson"
 curl "$URL" -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en,en-US;q=0.5' --compressed -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > deter-amazon-cloud-month.json
 
 # download daily data to work local
-URL="http://terrabrasilis.dpi.inpe.br/geoserver/deter-amz/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=all_daily_d&OUTPUTFORMAT=application%2Fjson"
+URL="${BASE_URL}/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=all_daily_d&OUTPUTFORMAT=application%2Fjson"
 curl "$URL" -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en,en-US;q=0.5' --compressed -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > deter-amazon-daily.json
 
 # download month aggregated data to work local
-URL="http://terrabrasilis.dpi.inpe.br/geoserver/deter-amz/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=month_d&OUTPUTFORMAT=application%2Fjson"
+URL="${BASE_URL}/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=month_d&OUTPUTFORMAT=application%2Fjson"
 curl "$URL" -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en,en-US;q=0.5' --compressed -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > deter-amazon-month.json
 
 # download the json for updated date information (DISABLED)
