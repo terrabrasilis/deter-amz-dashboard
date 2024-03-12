cbc746088b86fc4958067485e8ea733553a5c2ca
andre-carvalho <afacarvalho@yahoo.com.br>
2024-03-12 20:43:17 -0300

    Fix an error prevention code

--- webapp/js/deter-amazon-daily.js
+++ webapp/js/deter-amazon-daily.js
@@ -112,7 +112,7 @@ var graph={
 		}
 		else{
 			let layer_name=(typeof Authentication!="undefined"&&Authentication.hasToken())?("last_date"):("updated_date");
-			let url="http://terrabrasilis.dpi.inpe.br/geoserver/"+downloadCtrl.getProject()+"/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME="+layer_name+"&OUTPUTFORMAT=application%2Fjson";
+			let url=downloadCtrl.getTerraBrasilisHref()+"/geoserver/"+downloadCtrl.getProject()+"/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME="+layer_name+"&OUTPUTFORMAT=application%2Fjson";
 			d3.json(url, function(jsonResponse){
 				let dt=((jsonResponse&&jsonResponse.features[0].properties)?( (new Date(jsonResponse.features[0].properties.updated_date+'T21:00:00.000Z')).toLocaleDateString(Lang.language) ):('?') );
 				d3.select("#updated_date").html(' '+dt);
@@ -326,7 +326,8 @@ var graph={
 		 */
 		// if(!graph.dimensions["date"].hasCurrentFilter()){
 		
-		if(new Date(graph.dimensions.date.bottom(1)[0].timestamp)<graph.dateFilterRange[0]){
+		if(graph.dimensions.date.bottom(1).length &&
+		new Date(graph.dimensions.date.bottom(1)[0].timestamp)<graph.dateFilterRange[0]){
 			graph.setDateRangeOnDataset(graph.dateFilterRange[0],graph.dateFilterRange[1]);
 		}
 		let data = graph.areaByClass.all();
